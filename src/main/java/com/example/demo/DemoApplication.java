0d76568ad91d34c64de3b11ccce284af0107d3b6
Victor Lee <victorlee0505@gmail.com>
2023-05-09 13:15:06 -0400

    log: app started
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello World"); // We don't recommend sysout
+		log.info("My demo app started!");
 	}
 
 }
