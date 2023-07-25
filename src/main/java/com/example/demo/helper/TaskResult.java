7a8e1dba9a3ec9f1580d4f64cc5da19cef21e78b
Victor Lee <victorlee0505@gmail.com>
2023-07-25 15:59:58 -0400

    create mock long running task using different task executor
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

---
+++ src/main/java/com/example/demo/helper/TaskResult.java
@@ -0,0 +1,17 @@
+package com.example.demo.helper;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class TaskResult {
+    String message;
+    boolean status;
+}
