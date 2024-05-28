362e82b2cef7d4f7432b27b13fe0311f532226fb
Victor Lee <victorlee0505@gmail.com>
2024-05-28 16:29:38 -0400

    EmployeesController: use EmployeesService, make EmployeeAddRequest all fields mandatory
    Signed-off-by: Lee <victor.k.lee@ontario.ca>
    Signed-off-by: Victor Lee <victorlee0505@gmail.com>

--- src/main/java/com/example/demo/controller/EmployeesController.java
+++ src/main/java/com/example/demo/controller/EmployeesController.java
@@ -8,36 +8,42 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.controller.model.Employee;
 import com.example.demo.controller.model.EmployeeAddRequest;
 import com.example.demo.controller.model.EmployeeAddResponse;
+import com.example.demo.service.EmployeesService;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class EmployeesController implements EmployeesApi {
+
+    private final EmployeesService employeesService;
+
     @Override
     public ResponseEntity<EmployeeAddResponse> addEmployee(@Valid EmployeeAddRequest employeeAddRequest) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addEmployee'");
+        log.info("Adding employee: {}", employeeAddRequest);
+        return ResponseEntity.ok(new EmployeeAddResponse().employee(employeesService.addEmployee(employeeAddRequest)));
     }
 
     @Override
     public ResponseEntity<List<Employee>> getAllEmployees() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllEmployees'");
+        log.info("Getting all employees");
+        return ResponseEntity.ok(employeesService.getAllEmployees());
     }
 
     @Override
     public ResponseEntity<Employee> getEmployeeById(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEmployeeById'");
+        log.info("Getting employee by id: {}", id);
+        return ResponseEntity.ok(employeesService.getEmployeeById(Long.valueOf(id)));
     }
 
     @Override
     public ResponseEntity<List<Employee>> getEmployeesByServiceYearsLessThan(@NotNull @Valid Integer serviceYears) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEmployeesByServiceYearsLessThan'");
+        log.info("Getting employees by service years less than: {}", serviceYears);
+        return ResponseEntity.ok(employeesService.getEmployeesByServiceYearsLessThan(serviceYears));
     }
 
 }
