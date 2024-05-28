dfde6e6bbd964918dc2a8f9f1a334905f30ca7ad
Victor Lee <victorlee0505@gmail.com>
2024-05-28 16:29:42 -0400

    TaskController: use TaskProcessService
    Signed-off-by: Lee <victor.k.lee@ontario.ca>

--- src/main/java/com/example/demo/task/controller/TaskController.java
+++ src/main/java/com/example/demo/task/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.demo.task.controller;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,42 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TaskController implements TaskApi {
 
+    private final TaskProcessService taskProcessService;
+
+    /**
+     * GET /task/run : to run all tasks
+     * 
+     * http://localhost:8080/task/run
+     * http://localhost:8080/task/run?async=true
+     */
     @Override
     public ResponseEntity<Void> getAllTasks(@Valid Boolean async) {
 
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllTasks'");
+        log.info("getAllTasks called with async={}", async);
+
+        CompletableFuture.runAsync(() -> {
+            log.info("/work/multi is running");
+            final long start = System.currentTimeMillis();
+            int counter = 0;
+            List<TaskResult> results = null;
+            if (async) {
+                results = taskProcessService.multiThreadsThreadworkGenerator();
+            } else {
+                results = taskProcessService.singleThreadworkGenerator();
+            }
+            for (TaskResult taskResult : results) {
+                if (!taskResult.isStatus()) {
+                    counter++;
+                }
+            }
+            final long end = System.currentTimeMillis();
+            final long duration = (end - start) / 1000;
+            String result = "Duration: " + duration + "s, Failed task: " + counter;
+            log.info(result);
+            log.info("/work/multi Finished");
+        });
+
+        return ResponseEntity.accepted().build();
     }
 
 }
